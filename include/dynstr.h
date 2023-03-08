0f2bf85be8deb008549e0099cb09bdc95f808c47
Xavier Del Campo Romero <xavi.dcr@tutanota.com>
2023-03-08 01:18:47 +0100

    Update copyright notice

--- include/dynstr.h
+++ include/dynstr.h
@@ -1,5 +1,5 @@
 /*
-   Copyright 2020 Xavier Del Campo Romero
+   Copyright 2020-2023 Xavier Del Campo Romero
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
